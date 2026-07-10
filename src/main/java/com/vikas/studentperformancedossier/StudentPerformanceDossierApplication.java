4dc85617c2d709e282028e90850458fdf84dfdb2
Vikas Goyal <vikas.datascience2023@gmail.com>
2026-07-10 19:57:09 +0530

    Initial commit

---
+++ src/main/java/com/vikas/studentperformancedossier/StudentPerformanceDossierApplication.java
@@ -0,0 +1,13 @@
+package com.vikas.studentperformancedossier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPerformanceDossierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentPerformanceDossierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vikas/studentperformancedossier/StudentPerformanceDossierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.studentperformancedossier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPerformanceDossierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
